e62f8a42f482f3b76f349fe68e833721d91ae50d
Armando Cordova <jarmandocp@hotmail.com>
2022-03-21 01:20:34 -0400

    enhance ascii position renderer test

--- src/test/java/ch/astorm/jchess/util/ASCIIPositionRendererTest.java
+++ src/test/java/ch/astorm/jchess/util/ASCIIPositionRendererTest.java
@@ -9,17 +9,66 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class ASCIIPositionRendererTest {
+    private final String separator = System.lineSeparator();
+    private final String expectedOutput = "" +
+            "     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" +  separator +
+            "     :         :::::::::    _    :::www:::   _+_   ::::_::::         ::::::::::" + separator +
+            "  _  :  |=|=|  :: _,, ::   (/)   :::)#(:::   )#(   :::(/):::   _,,   ::|=|=|:::" + separator +
+            " (_) :   |#|   ::\"- \\~::   |#|   :::|#|:::   |#|   :::|#|:::  \"- \\~  :::|#|::::" + separator +
+            " (_) :   |#|   :::|#|:::   |#|   :::|#|:::   |#|   :::|#|:::   |#|   :::|#|::::" + separator +
+            "     :  /###\\  ::/###\\::  /###\\  ::/###\\::  /###\\  ::/###\\::  /###\\  ::/###\\:::" + separator +
+            "     ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "  __ :::::():::    ()   ::::():::    ()   ::::():::    ()   ::::():::    ()   :" + separator +
+            "   / :::::)(:::    )(   ::::)(:::    )(   ::::)(:::    )(   ::::)(:::    )(   :" + separator +
+            "  /  ::::/##\\::   /##\\  :::/##\\::   /##\\  :::/##\\::   /##\\  :::/##\\::   /##\\  :" + separator +
+            "     ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "  /  :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            " (_) :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "     ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "  _  ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            " |_  ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "  _) ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "     ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "   . :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "  /| :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            " '-| :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "     ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "  _  ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "  _) ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "  _) ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "     ::::::::::         :::::::::         :::::::::         :::::::::         :" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "  _  :    ()   ::::():::    ()   ::::():::    ()   ::::():::    ()   ::::()::::" + separator +
+            "   ) :    )(   ::::)(:::    )(   ::::)(:::    )(   ::::)(:::    )(   ::::)(::::" + separator +
+            "  /_ :   /__\\  :::/__\\::   /__\\  :::/__\\::   /__\\  :::/__\\::   /__\\  :::/__\\:::" + separator +
+            "     :         :::::::::         :::::::::         :::::::::         ::::::::::" + separator +
+            "     ::::::::::         ::::_::::   www   :::_+_:::    _    :::::::::         :" + separator +
+            "     :::|_|_|::   _,,   :::(/):::   ) (   :::) (:::   (/)   :: _,, ::  |_|_|  :" + separator +
+            "  /| ::::| |:::  \"- \\~  :::| |:::   | |   :::| |:::   | |   ::\"- \\~::   | |   :" + separator +
+            "   | ::::| |:::   | |   :::| |:::   | |   :::| |:::   | |   :::| |:::   | |   :" + separator +
+            "     :::/___\\::  /___\\  ::/___\\::  /___\\  ::/___\\::  /___\\  ::/___\\::  /___\\  :" + separator +
+            "     ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" + separator +
+            "                   _        _        _        __       __       _              " + separator +
+            "         /\\       |_)      /        | \\      |_       |_       /        |_|    " + separator +
+            "        /--\\      |_)      \\_       |_/      |__      |        \\_?      | |    " + separator +
+            "                                                                               " + separator +
+            separator;
 
     @Test
     public void testRendering() {
         JChessGame game = JChessGame.newGame();
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try(PrintStream stream = new PrintStream(baos, true, StandardCharsets.UTF_8)) {
+        try(PrintStream stream = new PrintStream(baos, true, StandardCharsets.US_ASCII)) {
             ASCIIPositionRenderer.render(stream, game.getPosition());
         }
 
-        String str = new String(baos.toByteArray(), StandardCharsets.UTF_8);
-        assertEquals(3681, str.length());
+        String str = baos.toString(StandardCharsets.US_ASCII);
+        assertEquals(expectedOutput, str);
     }
 }
