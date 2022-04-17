80dc12b4669b4e952f37b6f3a9f73338c9628889
Cédric Tabin <cedric.tabin@gmail.com>
2022-04-17 09:18:42 +0200

    Minor code cleanup in tests

--- src/test/java/ch/astorm/jchess/util/ASCIIPositionRendererTest.java
+++ src/test/java/ch/astorm/jchess/util/ASCIIPositionRendererTest.java
@@ -2,15 +2,13 @@
 package ch.astorm.jchess.util;
 
 import ch.astorm.jchess.JChessGame;
-
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Objects;
-
+import org.apache.commons.io.IOUtils;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
@@ -20,19 +18,14 @@ public class ASCIIPositionRendererTest {
         JChessGame game = JChessGame.newGame();
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try(PrintStream stream = new PrintStream(baos, true, StandardCharsets.US_ASCII)) {
-            ASCIIPositionRenderer.render(stream, game.getPosition());
-        }
-
+        try(PrintStream stream = new PrintStream(baos, true, StandardCharsets.US_ASCII)) { ASCIIPositionRenderer.render(stream, game.getPosition()); }
         String str = baos.toString(StandardCharsets.US_ASCII);
-        assertEquals(getExpectedOutput(), str);
-    }
-
-    private String getExpectedOutput() throws URISyntaxException, IOException {
-        URL expectedOutputTemplateURL = getClass().getResource("ASCIIPositionTemplate.txt");
-        Objects.requireNonNull(expectedOutputTemplateURL);
-        return Files
-                .readString(Path.of(expectedOutputTemplateURL.toURI()))
-                .replaceAll("separator", System.lineSeparator());
+        
+        String expected;
+        try(InputStream is = ASCIIPositionRendererTest.class.getResourceAsStream("ASCIIPositionTemplate.txt")) {
+            expected = IOUtils.toString(is, StandardCharsets.US_ASCII).replace("\n", System.lineSeparator());
+        }
+        
+        assertEquals(expected, str);
     }
 }
