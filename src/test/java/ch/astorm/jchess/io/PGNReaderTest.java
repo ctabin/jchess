04c082d65d83e4df56bcb21945e1e8704dcc9fe1
Joey De Pauw <joeydepauw@gmail.com>
2023-07-20 10:17:26 +0200

    added tests for pgn games where no moves were played yet (edge cases)

--- src/test/java/ch/astorm/jchess/io/PGNReaderTest.java
+++ src/test/java/ch/astorm/jchess/io/PGNReaderTest.java
@@ -3,13 +3,17 @@ package ch.astorm.jchess.io;
 
 import ch.astorm.jchess.JChessGame;
 import ch.astorm.jchess.JChessGame.Status;
+import ch.astorm.jchess.core.entities.Pawn;
 import ch.astorm.jchess.io.PGNReader.PGNReaderException;
 import java.io.StringReader;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
+
 
 public class PGNReaderTest {
     @Test
@@ -160,5 +164,66 @@ public class PGNReaderTest {
         } catch(IllegalStateException pe) {
             /* ok */
         }
+    }
+
+    /**
+     * From <a href="https://en.wikipedia.org/wiki/Portable_Game_Notation">wikipedia</a>:
+     * "If the game result is anything other than *, the result is repeated at the end of the movetext."
+     */
+    @Test
+    public void testOngoingGameNoAsteriskAtEnd() throws Exception {
+        String pgn =    "[Event \"Wch U20\"]\n" +
+                "[Site \"Kiljava\"]\n" +
+                "[Date \"1984.??.??\"]\n" +
+                "[Round \"?\"]\n" +
+                "[White \"Anand, Viswanathan\"]\n" +
+                "[Black \"Wolff, Patrick G\"]\n" +
+                "[Result \"*\"]\n" +
+                "[WhiteElo \"2285\"]\n" +
+                "[BlackElo \"2225\"]\n" +
+                "[ECO \"B09\"]\n" +
+                "\n" +
+                "1.e4 d6 2.d4 d5";
+        PGNReader parser = new PGNReader(new StringReader(pgn));
+        JChessGame game = parser.readGame();
+        assertNotNull(game);
+        assertEquals(game.getMetadata().get("Result"), "*");
+        assertEquals(game.getStatus(), Status.NOT_FINISHED);
+        assertTrue(game.get("e4") instanceof Pawn);
+    }
+
+    @Test
+    public void testGameNoMovesWithMetadata() throws Exception {
+        checkGameNoMovesWithMetadata("1-0", Status.WIN_WHITE);
+        checkGameNoMovesWithMetadata("1/2-1/2", Status.DRAW);
+        checkGameNoMovesWithMetadata("*", Status.NOT_FINISHED);
+        checkGameNoMovesWithMetadata("", Status.NOT_FINISHED);
+    }
+
+    private void checkGameNoMovesWithMetadata(String result, Status status) throws Exception{
+        // special case for end of movelist result for *.
+        String resultEol = result;
+        result = result.isEmpty() ? "*" : result;
+
+        String pgn =    "[Event \"Wch U20\"]\n" +
+                "[Site \"Kiljava\"]\n" +
+                "[Date \"1984.??.??\"]\n" +
+                "[Round \"?\"]\n" +
+                "[White \"Anand, Viswanathan\"]\n" +
+                "[Black \"Wolff, Patrick G\"]\n" +
+                "[Result \"" + result + "\"]\n" +
+                "[WhiteElo \"2285\"]\n" +
+                "[BlackElo \"2225\"]\n" +
+                "[ECO \"B09\"]\n" +
+                "\n" +
+                "" + resultEol; // no moves, only result
+        PGNReader parser = new PGNReader(new StringReader(pgn));
+        JChessGame game = parser.readGame();
+        assertNotNull(game);
+        // check some metadata
+        assertEquals(game.getMetadata().get("White"), "Anand, Viswanathan");
+        assertEquals(game.getMetadata().get("Black"), "Wolff, Patrick G");
+        assertEquals(game.getMetadata().get("Result"), result);
+        assertEquals(game.getStatus(), status);
     }
 }
